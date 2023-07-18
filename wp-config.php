ee278ec04ee53d5b85dfe87861f00ff94c245259
spiderdunia <spiderdunia.com@gmail.com>
2023-07-18 19:08:07 +0000

    new

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'u717168193_sdmain1' );
+define( 'DB_NAME', 'u717168193_sdmain' );
 
 /** Database username */
 
